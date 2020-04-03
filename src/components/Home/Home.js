13772bb42befbf62db264ff6bb03e97e31fe8665
syadallah <syadallah@gmail.com>
2020-04-03 13:41:24 -0400

    Update home.js <h2>

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,6 +9,7 @@ const Home = () => (
     <h2>Hppiness is...
       <br></br>
     Receving what you
+      <br></br>
     ordered online.
     </h2>
     <Link to="/products" className="btn btn-danger startShopping">Start Shopping</Link>
