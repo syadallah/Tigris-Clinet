1c38984f713a0caae948276c96633b692f8c522e
syadallah <syadallah@gmail.com>
2020-06-29 21:17:29 -0400

    Use filter to search for product

--- src/components/IndexProducts/IndexProducts.js
+++ src/components/IndexProducts/IndexProducts.js
@@ -9,7 +9,6 @@ import './IndexProducts.css'
 class Products extends Component {
   constructor () {
     super()
-
     // Useful constructors have STATE
     this.state = {
       products: null,
@@ -22,30 +21,41 @@ class Products extends Component {
     axios(`${apiUrl}/products`)
       .then(res => {
         this.setState({ products: res.data.products }, () => {
-          console.log(this.state)
         })
       })
       .catch(console.error)
   }
-
+  handleChange = (event) => {
+    this.setState({ value: event.target.value })
+    let productSearch
+    if (this.state.value) {
+      productSearch = this.state.products.filter(product => (
+        product.name === (event.target.value)
+      ))
+      console.log(this.state.value)
+      console.log(productSearch)
+      return productSearch
+    }
+  }
   render () {
     // Destructuring
   //  const { products } = this.state
-
     let productHtml
-    let productSearch
-    const handleChange = event => {
-      this.setState({ value: event.target.value })
-      productSearch = this.state.products.filter(product => (
-        product.includes(event.target.value)
-      ))
-      console.log(this.state.value)
-    }
 
     if (this.state.products) {
       // We have products!
 
       productHtml = this.state.products.map(product => (
+        <div className= "container" key={product.id}>
+          <div className='product'>
+            <h2 className='header'>{`${product.name}`}</h2>
+            <h6 className='description'>{`${product.description}`}</h6>
+            <h6 className='price'>{`${product.price}`} $</h6>
+          </div>
+        </div>
+      ))
+    } else if (productSearch) {
+      productSearched = productSearch.map(product => (
         <div className= "container" key={product.id}>
           <div className='product'>
             <h2 className='header'>{`${product.name}`}</h2>
@@ -68,15 +78,13 @@ class Products extends Component {
                 name="name"
                 placeholder="Search Products.."
                 value={this.state.value}
-                onChange={handleChange}
+                onChange={this.handleChange}
                 type="string"
               />
             </Form.Group>
           </Form.Row>
         </Form>
-
         <ul>
-          {productSearch}
           {productHtml}
         </ul>
       </Layout>
